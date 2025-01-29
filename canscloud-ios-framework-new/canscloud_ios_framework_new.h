7e8bba677d68fecdb003a5b22959cc910c1fa716
koiMobileDev <111335561+koiMobileDev@users.noreply.github.com>
2025-01-29 10:41:17 +0700

    Initial Commit

---
+++ canscloud-ios-framework-new/canscloud_ios_framework_new.h
@@ -0,0 +1,18 @@
+//
+//  canscloud_ios_framework_new.h
+//  canscloud-ios-framework-new
+//
+//  Created by koi on 29/1/2568 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for canscloud_ios_framework_new.
+FOUNDATION_EXPORT double canscloud_ios_framework_newVersionNumber;
+
+//! Project version string for canscloud_ios_framework_new.
+FOUNDATION_EXPORT const unsigned char canscloud_ios_framework_newVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <canscloud_ios_framework_new/PublicHeader.h>
+
+
---
+++ canscloud-ios-framework-newTests/canscloud_ios_framework_newTests.swift
@@ -0,0 +1,17 @@
+//
+//  canscloud_ios_framework_newTests.swift
+//  canscloud-ios-framework-newTests
+//
+//  Created by koi on 29/1/2568 BE.
+//
+
+import Testing
+@testable import canscloud_ios_framework_new
+
+struct canscloud_ios_framework_newTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
