4e2a52bac0677b153d0053ef7992886166cc3e77
koiMobileDev <111335561+koiMobileDev@users.noreply.github.com>
2025-01-30 00:28:05 +0700

    WIP: Call back data

---
+++ canscloud-ios-framework-new/CansCenter.swift
@@ -0,0 +1,14 @@
+//
+//  CansCenter.swift
+//  canscloud-ios-framework-new
+//
+//  Created by koi on 29/1/2568 BE.
+//
+
+import Foundation
+
+public class CansCenter {
+    public static func login() -> String {
+        return "Hello CansCenter"
+    }
+}
