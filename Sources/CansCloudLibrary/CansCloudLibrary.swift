59d13547e44e862d7f69c96700bd93477ab16785
koiMobileDev <111335561+koiMobileDev@users.noreply.github.com>
2025-01-29 14:32:08 +0700

    WIP: Add class

--- Sources/CansCloudLibrary/CansCloudLibrary.swift
+++ Sources/CansCloudLibrary/CansCloudLibrary.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import Foundation
+
+public class CansCloudLibrary {
+    public static func login() {
+        print("Hello CansCloudLibrary")
+    }
+}
